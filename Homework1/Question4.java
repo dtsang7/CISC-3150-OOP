5c17cb66f67cd52268f5e501ea24767d15a913f0
dtsang7 <dtsang7@yahoo.com>
2017-09-05 21:07:43 -0400

    Add files via upload

---
+++ Homework1/Question4.java
@@ -0,0 +1,17 @@
+/*
+*	Reads input from system.in and prints the contents 
+*	to the screen.
+*/
+import java.util.*;
+
+public class Question4{
+	
+	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+
+		while(input.hasNext()){
+			System.out.println(input.next());
+		}
+	}
+}
