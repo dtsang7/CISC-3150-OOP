13eb8c04f6c410ab23eca88bfc28097952b0cff4
dtsang7 <dtsang7@yahoo.com>
2017-09-05 21:07:13 -0400

    Add files via upload

---
+++ Homework1/Question3.java
@@ -0,0 +1,15 @@
+/*
+*	Generate a random integer between 1 and 12 and
+*	display the english name of that month.
+*/
+public class Question3{
+	
+	public static void main(String[] args){
+		
+		String[] months = {"January","February","March","April","May","June","July",
+							"August","September","October","November","December"};
+		
+		int random = (int)(Math.random()*12);
+		System.out.println(months[random]);
+	}
+}
