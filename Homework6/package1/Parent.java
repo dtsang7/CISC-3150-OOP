de770f656e8178b141dcb65da538f8bbb88b8a0b
Chi Pok Dersel Tsang <dtsang@CTsang-MBP.fios-router.home>
2017-10-04 01:56:15 -0400

    add homework 6

---
+++ Homework6/package1/Parent.java
@@ -0,0 +1,6 @@
+package package1;
+
+public class Parent{
+	protected String variable = "From parent.";
+
+}
---
+++ Homework6/package2/Child.java
@@ -0,0 +1,11 @@
+package package2;
+
+import package1.Parent;
+
+public class Child extends Parent{
+
+	public static void main(String[] args){
+			Child c = new Child();
+			System.out.println(c.variable);
+	}
+}
